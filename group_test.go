2be38bc353fd0f0500596965a86325268f224b33
xj <xiej@medlinker.com>
2018-07-30 17:01:36 +0800

    完善群组相关测试，修改bug

--- group_test.go
+++ group_test.go
@@ -1,7 +1,9 @@
 package tim
 
 import (
+	"math/rand"
 	"testing"
+	"time"
 )
 
 func TestCreateGroup(t *testing.T) {
@@ -33,6 +35,38 @@ func TestAddGroupMember(t *testing.T) {
 		t.Error(resp.ErrorInfo)
 	}
 }
+
+func TestDeleteGroupMember(t *testing.T) {
+	resp, err := testTimAPP.DeleteGroupMember("go1", []string{"10"}, "测试", 1)
+	if err != nil {
+		t.Error(err)
+	}
+	if resp.ActionStatus == ResponseFail {
+		t.Error(resp.ErrorInfo)
+	}
+}
+func TestImportGroupMember(t *testing.T) {
+	m1 := ImportMemberAccount{
+		JoinTime:     time.Date(2018, 7, 24, 12, 0, 0, 0, utc8).Unix(),
+		UnreadMsgNum: 10,
+	}
+	m1.MemberAccount = "10"
+	m1.Role = "Admin"
+
+	m2 := ImportMemberAccount{
+		JoinTime:     time.Date(2018, 7, 28, 12, 0, 0, 0, utc8).Unix(),
+		UnreadMsgNum: 1,
+	}
+	m2.MemberAccount = "notexists"
+	resp, err := testTimAPP.ImportGroupMember("go1", []ImportMemberAccount{m1, m2})
+	if err != nil {
+		t.Error(err)
+	}
+	if resp.ActionStatus == ResponseFail {
+		t.Error(resp.ErrorInfo)
+	}
+	t.Log(resp.MemberList)
+}
 func TestImportGroup(t *testing.T) {
 	def := GroupDefine{
 		GroupID:         "go1",
@@ -62,4 +96,52 @@ func TestDestroyGroup(t *testing.T) {
 	if resp.ActionStatus == ResponseFail {
 		t.Error(resp.ErrorCode, resp.ErrorInfo)
 	}
+}
+
+func TestImportGroupMsg(t *testing.T) {
+	m1 := Message{
+		FromAccount: "1",
+		Random:      rand.Int31(),
+		SendTime:    time.Date(2018, 7, 30, 16, 12, 3, 0, utc8).Unix(),
+		MsgBody: []MsgElement{
+			MsgElement{
+				MsgType: TIMText,
+				MsgContent: MsgText{
+					Text: "import message1ERER",
+				},
+			},
+		},
+	}
+	resp, err := testTimAPP.ImportGroupMsg("go1", []Message{m1})
+	if err != nil {
+		t.Error(err)
+	}
+	if resp.ActionStatus == ResponseFail {
+		t.Error(resp.ErrorCode, resp.ErrorInfo)
+	}
+	t.Log(resp)
+}
+
+func TestSendGroupMsg(t *testing.T) {
+	text := MsgElement{
+		MsgType: TIMText,
+		MsgContent: MsgText{
+			Text: "代发群消息",
+		},
+	}
+	msgGroup := GroupMessage{
+		GroupID:     "go1",
+		FromAccount: "1",
+		Random:      rand.Int31(),
+		MsgBody:     []MsgElement{text},
+	}
+	resp, err := testTimAPP.SendGroupMsg(msgGroup)
+
+	if err != nil {
+		t.Error(err)
+	}
+	if resp.ActionStatus == ResponseFail {
+		t.Error(resp.ErrorInfo)
+	}
+	t.Log(resp.MsgSeq)
 }
